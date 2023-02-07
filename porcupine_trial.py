4fdaf6d108ac71375b0b5b36525791e141dd9343
kmilgrim <kmilgrim@bu.edu>
2023-02-07 17:48:45 -0500

    currently not working, seems to be an issue with imports and dependencies

--- porcupine_trial.py
+++ porcupine_trial.py
@@ -1,10 +1,27 @@
 import pvporcupine
+from porcupine import Porcupine
 import os
 from dotenv import load_dotenv
 
 load_dotenv()
 
-# Porcupine access key
-access_key = os.getenv("PORCUPINE_ACCESS_KEY")
-print(access_key)
+porcupine = porcupine.create(
+    access_key = os.getenv("PORCUPINE_ACCESS_KEY"),
+    keywords = ['picovoice', 'bumblebee']
+)
 
+def get_next_audio_frame():
+  pass
+
+
+while True:
+  audio_frame = get_next_audio_frame()
+  keyword_index = porcupine.process(audio_frame)
+  if keyword_index == 0:
+      # detected `porcupine`
+      print(1)
+  elif keyword_index == 1:
+      # detected `bumblebee`
+      print(2)
+
+porcupine.delete()
